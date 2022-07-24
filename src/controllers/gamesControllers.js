4324e9c6cf529665ad1c7f4e563f6b1d45ce04c0
Gadiel Zilto Azevedo <gad.azevedo@gmail.com>
2022-07-24 19:42:52 -0300

    create createGame function in gamesControllers.js

--- src/controllers/gamesControllers.js
+++ src/controllers/gamesControllers.js
@@ -4,7 +4,7 @@ export async function getGames (req, res) {
   const filterGame = req.query.name;
 
   try {
-    if(filterGame.length > 0) {
+    if(filterGame) {
       const { rows: game } = await connection.query(
         `SELECT games.*, categories.name as "categoryName" 
         FROM games JOIN categories 
@@ -27,10 +27,15 @@ export async function getGames (req, res) {
 }
 
 export async function createGame (req, res) {
-  
+  const game = res.locals.game;
   
   try {
-    
+    await connection.query(
+      `INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") 
+      VALUES ($1, $2, $3, $4, $5)`, 
+      [game.name, game.image, game.stockTotal, game.categoryId, game.pricePerDay]);
+
+    res.sendStatus(201);
   } catch (error) {
     console.error(error);
     res.sendStatus(500);
