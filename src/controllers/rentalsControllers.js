1ef2627edc38ec8c573e45fa0758927c53041483
Gadiel Zilto Azevedo <gad.azevedo@gmail.com>
2022-07-25 13:52:13 -0300

    create getRentals function in rentalsControllers.js

--- src/controllers/rentalsControllers.js
+++ src/controllers/rentalsControllers.js
@@ -3,6 +3,64 @@ import connection from "../dbStrategy/postgres.js";
 export async function getRentals (req, res) {
   const filterByCustomerId = req.query.customerId;
   const filterByGameId = req.query.gameId;
+
+  try {
+    const { rows: listCustomers } = await connection.query(
+      `SELECT id, name FROM customers`
+    );
+
+    const { rows: listGames } = await connection.query(
+      `SELECT games.id, games.name, games."categoryId", categories.name as "categoryName" 
+      FROM games
+      JOIN categories ON games."categoryId"=categories.id`
+    );
+
+    if(filterByCustomerId) {
+      const { rows: listRentals } = await connection.query(
+        `SELECT * FROM rentals WHERE rentals."customerId"=$1`, [filterByCustomerId]
+      );
+
+      const returnRentals = listRentals.map(rental => {
+        const customer = listCustomers.filter(customer => customer.id === rental.customerId);
+        const game = listGames.filter(game => game.id === rental.gameId);
+
+        return {...rental, customer: customer[0], game: game[0]};
+      })
+
+      res.status(200).send(returnRentals);
+
+    } else if(filterByGameId) {
+      const { rows: listRentals } = await connection.query(
+        `SELECT * FROM rentals WHERE rentals."gameId"=$1`, [filterByGameId]
+      );
+
+      const returnRentals = listRentals.map(rental => {
+        const customer = listCustomers.filter(customer => customer.id === rental.customerId);
+        const game = listGames.filter(game => game.id === rental.gameId);
+
+        return {...rental, customer: customer[0], game: game[0]};
+      })
+
+      res.status(200).send(returnRentals);
+
+    } else {
+      const { rows: listRentals } = await connection.query(
+        `SELECT * FROM rentals`
+      );
+
+      const returnRentals = listRentals.map(rental => {
+        const customer = listCustomers.filter(customer => customer.id === rental.customerId);
+        const game = listGames.filter(game => game.id === rental.gameId);
+
+        return {...rental, customer: customer[0], game: game[0]};
+      })
+        
+      res.status(200).send(returnRentals);
+    }
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
 }
 
 export async function createRental (req, res) {
